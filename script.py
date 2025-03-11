69d857f947967ce82a68e83cb52e546760ef6de4
keerthimalan <keerthimalan@gmail.com>
2025-03-11 16:45:17 +0530

    Initial commit

---
+++ script.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+relayPin = 11    # GPIO17
+delay = 30
+
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(relayPin, GPIO.OUT)
+    GPIO.output(relayPin, GPIO.HIGH)
+
+
+def open_valve():
+    GPIO.output(relayPin, GPIO.HIGH)
+
+
+def close_valve():
+    GPIO.output(relayPin, GPIO.HIGH)
+
+
+def destroy():
+    GPIO.output(relayPin, GPIO.HIGH)
+    GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    setup()
+    try:
+        open_valve()
+        time.sleep(delay)
+        close_valve()
+    except KeyboardInterrupt:
+        destroy()
+
