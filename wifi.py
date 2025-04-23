8b840a80c2a46437145936d409b0107b60969447
Keerthimalan Balakandan <keerthimalan@napses.com>
2025-04-23 17:33:36 +0530

    Fix

--- wifi.py
+++ wifi.py
@@ -1,30 +1,22 @@
 import requests
 import time
 
-url = "192.168.1.50"
+url = "http://192.168.1.50"
 
-# payload = 'bkm=321&off=OFF'
 headers = {
   'Content-Type': 'application/x-www-form-urlencoded'
 }
 
-# response = requests.request("POST", url, headers=headers, data=payload)
-
-# print(response.text)
 
 def open_valve():
-    # url = "192.168.1.50"
     payload = 'auth=321&state=ON'
-    # headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
     print(response.text)
 
 def close_valve():
-    # url = "192.168.1.50"
     payload = 'auth=321&state=OFF'
-    # headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
