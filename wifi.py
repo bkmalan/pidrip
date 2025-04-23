bf07a9d0fb1a723113b1718612b3d5b26afa0e44
Keerthimalan Balakandan <keerthimalan@napses.com>
2025-04-23 18:37:37 +0530

    Add telegram integration

--- wifi.py
+++ wifi.py
@@ -7,20 +7,46 @@ headers = {
   'Content-Type': 'application/x-www-form-urlencoded'
 }
 
+def send_message(msg):
+    BOT_TOKEN = "bot8123516038:AAHo3fl8CfC6oGx-unJ_jMRLLMK8c2CNyt0"
+    CHAT_ID = "-4722370003"
+
+    url = f"https://api.telegram.org/{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": msg
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+    try:
+        response = requests.post(url, json=payload, headers=headers)
+        response.raise_for_status()  # Raise an error for bad HTTP responses (4xx, 5xx)
+        # print(response.status_code)
+        # print(response.json())
+    except requests.exceptions.RequestException as e:
+        print(f"Telegram message failed: {e}")
+
 
 def open_valve():
     payload = 'auth=321&state=ON'
 
     response = requests.request("POST", url, headers=headers, data=payload)
-
+    print('watering')
     print(response.text)
+    send_message("Watering plants")
 
 def close_valve():
     payload = 'auth=321&state=OFF'
 
     response = requests.request("POST", url, headers=headers, data=payload)
+    print('done')
 
     print(response.text)
+    send_message("Watering complete")
+
 
 
 
